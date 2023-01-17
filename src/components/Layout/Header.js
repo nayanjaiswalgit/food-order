71c82f4db1e3c7588131fcda22609f10c514ecc6
Nayan <jaiswal2062@gmail.com>
2023-01-17 12:05:39 +0530

    version V1.0

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCardButton from "./HeaderCardButton";
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCardButton  onClick = {props.onShowCart}>Cart</HeaderCardButton>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="food"></img>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,17 @@
+import classes from './MealsSummary.module.css'
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To you </h2>
+      <p>
+        Choose Your Favourite meal from our broad selection of available meals
+        and enjoy a Delicious lunch of Dinner at Home.
+      </p>
+      <p>
+        All our meals are cooked with high-quality ingredients, just-in-time and
+        of course by experienced chefs!
+      </p>
+    </section>
+  );
+};
+export default MealsSummary;
